23729a60c1638d93db9310428a899d214564ef7a
Ronald Gustmann <103508049+RonaldEduardo@users.noreply.github.com>
2025-10-20 15:52:12 +0000

    hotfix: Compreensivo Organizacional Desenvolvimento Integração Gerência Operações - sistema CÓDIGO

---
+++ SistemaDocumentacao.java
@@ -0,0 +1,23 @@
+/**
+ * Sistema de Documentação Legada
+ * Última modificação: 3 anos atrás
+ * Status: Arquivado
+ */
+
+public class SistemaDocumentacao {
+    
+    // Sistema existe mas não executa
+    // Conta a história do que o código costumava fazer
+    // Frequentemente a maior mentira em projetos legados
+    
+    public void gerarRelatorio() {
+        // TODO: Implementação pendente
+        // Este método deveria funcionar mas...
+        // Veja o histórico git para a verdade
+    }
+    
+    private void validarEntrada(String entrada) {
+        // FIXME: Esta validação nunca funcionou
+        // Mas todo mundo finge que funciona
+    }
+}
